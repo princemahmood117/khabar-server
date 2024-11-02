14a13e0231221cc572e0e8e0bae30b0a6e9e13c1
princemahmood117 <princemahmood117@gmail.com>
2024-11-02 19:37:28 +0600

    add food done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("doctor running");
+  res.send("food running");
 });
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -105,6 +105,36 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+
+
+
+    
+    app.post('/food', async(req,res)=>{
+      const newFood = req.body; 
+      console.log(newFood);
+
+      const result = await serviceCollection.insertOne(newFood)
+      
+      res.send(result);
+  })
+
+   
+
+  app.get('/food', async(req , res) => {
+  const cursor = serviceCollection.find() 
+  const result = await cursor.toArray();
+  res.send(result)
+  
+  })
+
+    
+
+
+
+
+
+
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
